992ae87650d3ab944bf1b7b21316badafb496a23
Lisandro Guerra <lisandro.digital@gmail.com>
2023-02-06 14:54:55 +0000

    refactoring index creatind elements

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 import Controls from "./controls.js"
 import Timer from "./timer.js"
-
-const buttonPlay = document.querySelector('.play')
-const buttonPause = document.querySelector('.pause')
-const buttonSet = document.querySelector('.set')
-const buttonStop = document.querySelector('.stop')
-const buttonSoundOn = document.querySelector('.sound-on')
-const buttonSoundOff = document.querySelector('.sound-off')
-const minutesDisplay = document.querySelector('.minutes')
-const secondsDisplay = document.querySelector('.seconds')
-
+import {
+    buttonPlay,
+    buttonPause,
+    buttonSet,
+    buttonStop,
+    buttonSoundOn,
+    buttonSoundOff,
+    minutesDisplay,
+    secondsDisplay
+} from "./elements.js"
 
 
 const controls = Controls(
